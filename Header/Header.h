1a0820be1b347b5a6565a0238fbbb74a8351aebd
Leexiang <NeonLeexiang@users.noreply.github.com>
2019-03-31 15:32:12 +0800

    create header.h to store some typedef

---
+++ Header/Header.h
@@ -0,0 +1,34 @@
+//
+//  Header.h
+//  data_structure_and_algorithm
+//
+//  Created by NeonRocks on 2019/3/24.
+//  Copyright © 2019 NeonRocks. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+
+#include <stdio.h>
+
+//typedef before using
+typedef int Status;
+typedef int ElemType;
+
+//
+#define TURE 1
+#define FALSE 0
+#define OK 1
+#define ERROR 0
+#define INFEASIBLE -1
+#define OVERFLOW -2
+
+#define LIST_INIT_SIZE 100
+#define LISTINCREMENT 10
+#define MAXSIZE 1000
+
+
+#include <mm_malloc.h>
+
+
+#endif /* Header_h */
