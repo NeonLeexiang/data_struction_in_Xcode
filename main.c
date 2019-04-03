93eaa39dad71a848c414137fb723ef7ef2fe6c2d
Leexiang <NeonLeexiang@users.noreply.github.com>
2019-04-03 11:09:27 +0800

    Create main.c

---
+++ main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  data_structure_and_algorithm
+//
+//  Created by NeonRocks on 2019/3/24.
+//  Copyright © 2019 NeonRocks. All rights reserved.
+//
+
+#include <stdio.h>
+#include "SqList.h"
+
+int main() {
+    // insert code here...
+    SqListUse();
+    return 0;
+}
